038770a9f5f12df6e8a870456902e960745d1e86
aramg <aramg@users.noreply.github.com>
2023-03-13 16:00:14 -0700

    Enable Hotkey Support
    * Disable most existing options
    * Add Show/Hide hotkey pair
    * Add Source-specific screenshot hotkey
    ** (submit upstream)

--- UI/window-basic-settings.cpp
+++ UI/window-basic-settings.cpp
@@ -908,7 +908,6 @@ OBSBasicSettings::OBSBasicSettings(QWidget *parent)
 #ifdef DROIDCAM_OVERRIDE
 	ui->listWidget->setRowHidden(1, true); // Stream
 	ui->listWidget->setRowHidden(2, true); // Output
-	ui->listWidget->setRowHidden(5, true); // Hotkeys
 
 	#define HIDE_ITEM(item) \
 		if (item) item->setVisible(false)
@@ -2927,9 +2926,9 @@ void OBSBasicSettings::LoadHotkeySettings(obs_hotkey_id ignoreKey)
 			return true;
 
 		if (obs_scene_from_source(source))
-			scenes.emplace_back(source, label, hw);
+			scenes.emplace_back(move(source), label, hw);
 		else if (obs_source_get_name(source) != NULL)
-			sources.emplace_back(source, label, hw);
+			sources.emplace_back(move(source), label, hw);
 
 		return false;
 	};
@@ -2953,6 +2952,7 @@ void OBSBasicSettings::LoadHotkeySettings(obs_hotkey_id ignoreKey)
 			layout->addRow(label, hw);
 			break;
 
+		#if DROIDCAM_OVERRIDE==0
 		case OBS_HOTKEY_REGISTERER_ENCODER:
 			if (HandleEncoder(registerer, label, hw))
 				return;
@@ -2968,6 +2968,7 @@ void OBSBasicSettings::LoadHotkeySettings(obs_hotkey_id ignoreKey)
 				return;
 			break;
 
+		#endif
 		case OBS_HOTKEY_REGISTERER_SOURCE:
 			if (HandleSource(registerer, label, hw))
 				return;
