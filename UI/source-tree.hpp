dev47apps/obs-studio

Add DroidCam source battery label

UI/source-tree.hpp
@@ -50,7 +50,6 @@ class SourceTreeItem : public QWidget {
 		SubItem,
 	};
 
-	void DisconnectSignals();
 	void ReconnectSignals();
 
 	Type type = Type::Unknown;
@@ -67,6 +66,8 @@ private:
 	LockedCheckBox *lock = nullptr;
 	QHBoxLayout *boxLayout = nullptr;
 	QLabel *label = nullptr;
+	QLabel *batteryIcon = nullptr;
+	QLabel *batteryText = nullptr;
 
 	QLineEdit *editor = nullptr;
 
@@ -74,15 +75,7 @@ private:
 
 	SourceTree *tree;
 	OBSSceneItem sceneitem;
-	OBSSignal sceneRemoveSignal;
-	OBSSignal itemRemoveSignal;
-	OBSSignal groupReorderSignal;
-	OBSSignal selectSignal;
-	OBSSignal deselectSignal;
-	OBSSignal visibleSignal;
-	OBSSignal lockedSignal;
-	OBSSignal renameSignal;
-	OBSSignal removeSignal;
+	std::vector<OBSSignal> signalHandlers;
 
 	virtual void paintEvent(QPaintEvent *event) override;
 
@@ -97,6 +90,7 @@ private slots:
 	void VisibilityChanged(bool visible);
 	void LockedChanged(bool locked);
 	void Renamed(const QString &name);
+	void BatteryChanged(const QString &value, int alert);
 
 	void ExpandClicked(bool checked);
 

dev47apps/droidcam-obs-client

UI/source-tree.hpp
@@ -67,6 +67,8 @@ private:
 	LockedCheckBox *lock = nullptr;
 	QHBoxLayout *boxLayout = nullptr;
 	QLabel *label = nullptr;
+	QLabel *batteryIcon = nullptr;
+	QLabel *batteryText = nullptr;
 
 	QLineEdit *editor = nullptr;
 
@@ -74,15 +76,7 @@ private:
 
 	SourceTree *tree;
 	OBSSceneItem sceneitem;
-	OBSSignal sceneRemoveSignal;
-	OBSSignal itemRemoveSignal;
-	OBSSignal groupReorderSignal;
-	OBSSignal selectSignal;
-	OBSSignal deselectSignal;
-	OBSSignal visibleSignal;
-	OBSSignal lockedSignal;
-	OBSSignal renameSignal;
-	OBSSignal removeSignal;
+	std::vector<OBSSignal> sigs;
 
 	virtual void paintEvent(QPaintEvent *event) override;
 
@@ -97,6 +91,7 @@ private slots:
 	void VisibilityChanged(bool visible);
 	void LockedChanged(bool locked);
 	void Renamed(const QString &name);
+	void BatteryChanged(const QString &value, int alert);
 
 	void ExpandClicked(bool checked);
 